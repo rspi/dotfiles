50e51817eb58df5c6c3fdd002f4765e775325b7f
Rasmus Spiegelberg <2677747+rspi@users.noreply.github.com>
2025-01-08 16:08:44 +0100

    Show hidden files in Oil by default

--- config/nvim/lua/plugins/stuff.lua
+++ config/nvim/lua/plugins/stuff.lua
@@ -29,6 +29,9 @@ return {
 		"stevearc/oil.nvim",
 		config = function()
 			require("oil").setup({
+				view_options = {
+					show_hidden = true,
+				},
 				keymaps = {
 					["<C-f><C-f>"] = { "actions.close", mode = "n" },
 					["q"] = { "actions.close", mode = "n" },
