a5dce1ebf4aa18f85a0fdb994b1f962b3f702939
Rasmus Spiegelberg <2677747+rspi@users.noreply.github.com>
2025-02-10 16:02:46 +0100

    update dap

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -4,14 +4,28 @@ return {
 		dependencies = {
 			"rcarriga/nvim-dap-ui",
 			"nvim-neotest/nvim-nio",
+			"mfussenegger/nvim-dap-python",
 		},
 		config = function()
 			local dap = require("dap")
 			local ui = require("dapui")
+			local dap_python = require("dap-python").setup("<python venv path>")
 
 			require("dapui").setup()
+			table.insert(dap.configurations.python, {
+				type = "debugpy",
+				request = "attach",
+				name = "Remote debugger",
+				connect = {
+					port = 5678,
+					host = "127.0.0.1",
+				},
+				justMyCode = false,
+				django = true,
+				program = "${file}",
+			})
 
-			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+			vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
 			dap.listeners.before.attach.dapui_config = function()
 				ui.open()
 			end
