b4e27e643ba6d002c19348ddbc808b3074f84843
Rasmus Spiegelberg <2677747+rspi@users.noreply.github.com>
2025-02-13 11:52:19 +0100

    Update DAP setup
    * Remove dap UI which seems overkill
    * Add more keybindings

--- config/nvim/lua/plugins/dap.lua
+++ config/nvim/lua/plugins/dap.lua
@@ -2,16 +2,13 @@ return {
 	{
 		"mfussenegger/nvim-dap",
 		dependencies = {
-			"rcarriga/nvim-dap-ui",
 			"nvim-neotest/nvim-nio",
 			"mfussenegger/nvim-dap-python",
 		},
 		config = function()
 			local dap = require("dap")
-			local ui = require("dapui")
-			local dap_python = require("dap-python").setup("<python venv path>")
+			require("dap-python").setup()
 
-			require("dapui").setup()
 			table.insert(dap.configurations.python, {
 				type = "debugpy",
 				request = "attach",
@@ -25,19 +22,31 @@ return {
 				program = "${file}",
 			})
 
+			local onOpen = function()
+				dap.repl.open()
+				vim.keymap.set("n", "<leader>c", dap.run_to_cursor)
+				vim.keymap.set("n", "<up>", dap.step_back)
+				vim.keymap.set("n", "<down>", dap.step_over)
+				vim.keymap.set("n", "<right>", dap.step_into)
+				vim.keymap.set("n", "<left>", dap.step_out)
+				vim.keymap.set("n", "<CR>", dap.continue)
+			end
+
+			local onClose = function()
+				dap.repl.close()
+				vim.keymap.del("n", "<leader>c")
+				vim.keymap.del("n", "<up>")
+				vim.keymap.del("n", "<down>")
+				vim.keymap.del("n", "<right>")
+				vim.keymap.del("n", "<left>")
+				vim.keymap.del("n", "<CR>")
+			end
+
 			vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint)
-			dap.listeners.before.attach.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				ui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				ui.close()
-			end
+			dap.listeners.before.attach.dapui_config = onOpen
+			dap.listeners.before.launch.dapui_config = onOpen
+			dap.listeners.before.event_terminated.dapui_config = onClose
+			dap.listeners.before.event_exited.dapui_config = onClose
 		end,
 	},
 }
