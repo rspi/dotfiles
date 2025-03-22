d8224831a6432c77a66a98ff1ecb84d8d37ab612
Rasmus Spiegelberg <2677747+rspi@users.noreply.github.com>
2025-03-22 16:41:12 +0100

    lazy dev config

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -132,7 +132,12 @@ return {
 	{
 		"folke/lazydev.nvim",
 		ft = "lua",
-		opts = {},
+		opts = {
+			library = {
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
 	},
 
 	-- Formatters
