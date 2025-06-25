1332622770352dc18f5b8aa246ed0617292cb597
Rasmus Spiegelberg <2677747+rspi@users.noreply.github.com>
2025-06-25 11:54:01 +0200

    Fix telescope visual selection search that broke at some point. Seems to not be included in telescope anymore..

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -33,11 +33,31 @@ return {
 		local custom_pickers = require("telescope_pickers")
 		telescope.setup(opts)
 		telescope.load_extension("fzf")
-
 		local builtin = require("telescope.builtin")
+
+		local function get_visual_selection()
+			local current_clipboard_content = vim.fn.getreg('"')
+			vim.cmd('noau normal! "vy"')
+			local text = vim.fn.getreg("v")
+			vim.fn.setreg("v", {})
+			vim.fn.setreg('"', current_clipboard_content)
+			text = string.gsub(text, "\n", "")
+			if #text > 0 then
+				return text
+			else
+				return ""
+			end
+		end
+
+		local function grep_visual_selection()
+			local text = get_visual_selection()
+			builtin.grep_string({ search = text })
+		end
+
 		vim.keymap.set("n", "<C-f><C-p>", builtin.find_files, { desc = "Find files" })
 		vim.keymap.set("n", "<C-f><C-g>", custom_pickers.branch_files, { desc = "Find files on branch" })
-		vim.keymap.set({ "n", "v" }, "<C-f><C-w>", builtin.grep_string, { desc = "Search current word" })
+		vim.keymap.set({ "v" }, "<C-f><C-w>", grep_visual_selection, { desc = "Search current selection" })
+		vim.keymap.set({ "n" }, "<C-f><C-w>", builtin.grep_string, { desc = "Search current word" })
 		vim.api.nvim_create_autocmd("User", { pattern = "TelescopePreviewerLoaded", command = "setlocal number" })
 	end,
 }
